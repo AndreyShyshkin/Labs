09cfa6783f7950633904a56e1dd308a09274c4b2
Andrey_Shyshkin <shyshkinandrey06@gmail.com>
2024-09-28 18:01:29 +0300

    bugFix

--- op/report/vite.config.js
+++ op/report/vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
 		rollupOptions: {
 			input: {
 				main: resolve(__dirname, 'index.html'),
+				katalog: resolve(__dirname, 'pages/LabsKatalog/LabsKatalog.jsx'),
 			},
 		},
 	},
