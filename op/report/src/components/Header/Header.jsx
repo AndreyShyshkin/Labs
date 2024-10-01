4418419827fdc1c2933f0391069f89cab831966d
Andrey_Shyshkin <shyshkinandrey06@gmail.com>
2024-10-01 18:03:22 +0300

    bugFix

--- op/report/src/components/Header/Header.jsx
+++ op/report/src/components/Header/Header.jsx
@@ -54,7 +54,7 @@ export default function Header() {
 		<header className='absolute inset-x-0 top-0 z-50'>
 			<nav
 				aria-label='Global'
-				className='flex items-center justify-between py-6 lg:px-8 max-w-screen-xl mx-auto'
+				className='flex items-center justify-between py-6 px-6 lg:px-8 max-w-screen-xl mx-auto'
 			>
 				<div className='flex lg:flex-1'>
 					<Link to='/' className='-m-1.5 p-1.5'>
