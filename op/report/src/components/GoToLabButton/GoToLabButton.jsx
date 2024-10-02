032acb6f2969c051f2ab93506841c305a1fecb4f
Andrey_Shyshkin <shyshkinandrey06@gmail.com>
2024-10-02 20:21:22 +0300

    bugFix

--- op/report/src/components/GoToLabButton/GoToLabButton.jsx
+++ op/report/src/components/GoToLabButton/GoToLabButton.jsx
@@ -1,21 +1,21 @@
-import { Link, useLocation } from 'react-router-dom'
+import { Link, useLocation } from "react-router-dom";
 
 export default function GoToLabButton() {
-	const location = useLocation()
+  const location = useLocation();
 
-	function handleGoToLab() {
-		const pathParts = location.pathname.split('/')
+  function handleGoToLab() {
+    const pathParts = location.pathname.split("/");
 
-		if (pathParts.length > 1) {
-			const basePath = pathParts[1]
+    if (pathParts.length > 1) {
+      const basePath = pathParts[1];
 
-			return `/${basePath}`
-		}
-	}
+      return `/${basePath}`;
+    }
+  }
 
-	return (
-		<Link to={handleGoToLab()} className='text-gray-400 py-2'>
-			&lt; Повернутися до каталогу
-		</Link>
-	)
+  return (
+    <Link to={handleGoToLab()} className="text-gray-400 py-2 z-1 relative">
+      &lt; Повернутися до каталогу
+    </Link>
+  );
 }
